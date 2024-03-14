3ec96e6d46785ce4cd8c8cab5e40809b759d4fba
Carolyn Liu <carolynliu@carolyns-air.wifi.local.cmu.edu>
2024-03-14 16:07:22 -0400

    plus first attempt

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -29,8 +29,13 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("plus")) {
+    let i = query.indexOf(" plus ");
+    let arr = query.split(" plus ");
+    let num1 = arr[0].replace( /[^\d.]/g, '' );
+    let num2 = arr[1].replace( /[^\d.]/g, '' );
+    let res = num1 + num2;
     return (
-      "56"
+      res.toString()
     );
   }
 
